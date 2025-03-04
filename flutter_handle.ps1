5c9d2a70060b390d997d0bc832788cef4677805e
mrfrolk <ekaterita20@gmail.com>
2025-03-05 03:56:52 +0500

    init

---
+++ flutter_handle.ps1
@@ -0,0 +1,36 @@
+
+# после обновления пакета
+flutter pub cache clean
+flutter clean
+flutter pub get
+
+flutter pub add http
+
+flutter pub add flutter_riverpod
+flutter pub add riverpod_annotation
+flutter pub add dev:riverpod_generator
+flutter pub add dev:build_runner
+flutter pub add dev:custom_lint
+flutter pub add dev:riverpod_lint
+flutter pub add hooks_riverpod
+
+flutter pub add custom_lint --dev
+flutter pub add riverpod_lint --dev
+
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+dart run build_runner build
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner watch --delete-conflicting-outputs
+
+# Для добавления зависимостей в основной проект
+flutter pub add freezed_annotation
+flutter pub add json_annotation
+
+# Для добавления dev-зависимостей
+flutter pub add freezed --dev
+flutter pub add json_serializable --dev
+flutter pub add build_runner --dev
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("My App")));
+  }
+}
+
+
